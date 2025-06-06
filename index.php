e37655e1d3a27c51bd0764c2c33638986130fa9c
gosi <mailtosuresh.santhanam2004@gmail.com>
2025-06-06 11:53:28 +0300

    details_updated

--- index.php
+++ index.php
@@ -4,45 +4,40 @@
 	<title>MySQL Table Viewer</title>
 </head>
 <body>
-	<h1>MySQL Table Viewer-3</h1>
+	<h1>MySQL Table Viewer</h1>
 	<?php
-	    echo "Hello World";
 		// Define database connection variables
-		$servername = "ss-azuresqlserver.mysql.database.azure.com";
+		$servername = "ss-sqlserver.mysql.database.azure.com";
 		$username = "ss_sqlserver_admin";
 		$password = "Jks@4231";
-		$dbname = "ss_hrms_db";
+		$dbname = "hrms_db";
 		
-		echo $servername;
-		echo "xxxxxxxxxxxxxxxx";
 		// Create database connection
 		$conn = new mysqli($servername, $username, $password, $dbname);
-        echo "ss1";
-		echo $conn;
 		// Check connection
 		if ($conn->connect_error) {
-			echo "connecton Error";
+
 			die("Connection failed: " . $conn->connect_error);
 		}
+		
+		 // Query database for all rows in the table
+                $sql = "SELECT * FROM employees";
+                $result = $conn->query($sql);
 
-		// Query database for all rows in the table
-		$sql = "SELECT * FROM employee";
-		$result = $conn->query($sql);
+                if ($result->num_rows > 0) {
+                        // Display table headers
+                        echo "<table><tr><th>Employee Number</th><th>First Name</th><th>Email</th></tr>";
+                        // Loop through results and display each row in the table
+                        while($row = $result->fetch_assoc()) {
+                                echo "<tr><td>" . $row["emp_no"] . "</td><td>" . $row["first_name"] . "</td><td>" . $row["email_id"] . "</td></tr>";
+                        }
+                        echo "</table>";
+                } else {
+                        echo "0 results";
+                }
 
-		if ($result->num_rows > 0) {
-			// Display table headers
-			echo "<table><tr><th>Employee ID</th><th>Employee Name</th><th>Email</th></tr>";
-			// Loop through results and display each row in the table
-			while($row = $result->fetch_assoc()) {
-				echo "<tr><td>" . $row["employee_id"] . "</td><td>" . $row["employee_name"] . "</td><td>" . $row["email"] . "</td></tr>";
-			}
-			echo "</table>";
-		} else {
-			echo "0 results";
-		}
-
-		// Close database connection
-		$conn->close();
+                // Close database connection
+                $conn->close();
 	?>
 </body>
 </html>
